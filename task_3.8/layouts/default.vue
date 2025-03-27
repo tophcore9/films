ea3afeafd3c112a2b1be12e43b0a1d640457ca48
Oleksii Paziura <o.paziura@imcagro.com.ua>
2025-03-27 12:35:16 +0200

    Initial commit

---
+++ task_3.8/layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="_container">
+        <div class="_wrapper">
+            <Nav/>
+            <slot></slot>
+            <Footer/>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+import Nav from '~/components/Nav.vue';
+import Footer from '~/components/Footer.vue';
+
+export default defineComponent({
+    name: "default",
+    components: {
+        Nav,
+        Footer
+    }
+})
+</script>
+
+<style lang="scss">
+@use 'assets/styles/reset.css';
+
+._container {
+
+}
+._wrapper {
+
+}
+</style>
